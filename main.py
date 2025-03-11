d2cc96f4ce73a539151573bed07fe6b51a88ca20
Bum-Ho12 <bumhonisu@gmail.com>
2025-03-11 10:47:28 +0300

    enhanced main.py with day 2 assignment

--- main.py
+++ main.py
@@ -9,23 +9,34 @@ def main():
     if len(sys.argv) == 3 and sys.argv[1] == 'run' and sys.argv[2] == 'arithmetics':
         print(" Intro to Python Assignment: Week 3 Day 1 Assignment -> file is python_basics_week_3_day_1.py ")
         arithmetic_math_calculator()
+
+    # DSA command
+    if len(sys.argv) == 3 and sys.argv[1] == 'run' and sys.argv[2] == 'dsa':
+        print(" Intro to Python Assignment: Week 3 Day 2 Assignment -> file is python_basics_week_3_day_2.py ")
+
     # help command
     elif len(sys.argv) == 2 and sys.argv[1] == '--help':
         print("Usage: py main.py run <key_word>")
         print("Available key words:")
         print("  arithmetics - Runs the arithmetic calculator")
         print("  --help      - Displays this help message")
+
+    # --info command
     elif len(sys.argv) == 2 and sys.argv[1] == '--info':
         # files
         print(" Main where all executions are -> file is main.py ")
         print(" Intro to Python Assignment: Week 3 Day 1 Assignment -> file is python_basics_week_3_day_1.py ")
+        print(" Intro to Python Assignment: Week 3 Day 2 Assignment -> file is python_basics_week_3_day_2.py ")
         # commands
         print("For mor commands: ")
         print("For more help, type: py main.py --help")
+
+    # if errors
     else:
         print("Invalid command.")
         print("Usage: py main.py run <key_word>")
         print("For more help, type: py main.py --help")
+        print("For Information, type: py main.py --info")
 
 if __name__ == "__main__":
     main()
